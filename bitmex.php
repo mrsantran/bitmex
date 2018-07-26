3827ac370d2c6ece7a9d9df1bbe54971e38e6c67
San Trần <laptrinhvien2013@gmail.com>
2018-07-26 09:24:41 +0700

    Add method get instrument

--- bitmex.php
+++ bitmex.php
@@ -24,12 +24,15 @@ namespace BitmexPHP;
 class API {
 
     protected $ws = 'wss://www.bitmex.com/realtime?subscribe=';
+    protected $ws_test = "wss://testnet.bitmex.com/realtime?subscribe=";
     protected $api_key;
     protected $api_secret;
+    protected $env;
 
-    public function __construct($api_key = '', $api_secret = '') {
+    public function __construct($api_key = '', $api_secret = '', $env = true) {
         $this->api_key = $api_key;
         $this->api_secret = $api_secret;
+        $this->env = $env ? $this->ws : $this->ws_test;
     }
 
     public function trades($symbols, $callback) {
@@ -40,7 +43,7 @@ class API {
         $connector = new \Ratchet\Client\Connector($loop, $react);
         foreach ($symbols as $symbol) {
             $endpoint = 'trade' . ':' . $symbol;
-            $connector($this->ws . $endpoint)->then(function ( $ws ) use ($callback, $symbol, $loop, $endpoint ) {
+            $connector($this->env . $endpoint)->then(function ( $ws ) use ($callback, $symbol, $loop, $endpoint ) {
                 $ws->on('message', function ( $data ) use ($ws, $loop, $symbol, $callback, $endpoint ) {
                     $json = json_decode($data, true);
                     if (isset($json["data"])) {
@@ -53,6 +56,33 @@ class API {
                 });
             }, function ( $e ) use ($loop, $symbol ) {
                 echo "trade({$symbol})) Could not connect: {$e->getMessage()}" . PHP_EOL;
+                $loop->stop();
+            });
+        }
+        $loop->run();
+    }
+
+    public function instrument($symbols, $callback) {
+        if (!is_array($symbols))
+            $symbols = [$symbols];
+        $loop = \React\EventLoop\Factory::create();
+        $react = new \React\Socket\Connector($loop);
+        $connector = new \Ratchet\Client\Connector($loop, $react);
+        foreach ($symbols as $symbol) {
+            $endpoint = 'instrument' . ':' . $symbol;
+            $connector($this->env . $endpoint)->then(function ( $ws ) use ($callback, $symbol, $loop, $endpoint ) {
+                $ws->on('message', function ( $data ) use ($ws, $loop, $symbol, $callback, $endpoint ) {
+                    $json = json_decode($data, true);
+                    if (isset($json["data"])) {
+                        call_user_func($callback, $this, $symbol, $json["data"]);
+                    }
+                });
+                $ws->on('close', function ( $code = null, $reason = null ) use ($symbol, $loop ) {
+                    echo "instrument({$symbol}) WebSocket Connection closed! ({$code} - {$reason})" . PHP_EOL;
+                    $loop->stop();
+                });
+            }, function ( $e ) use ($loop, $symbol ) {
+                echo "instrument({$symbol})) Could not connect: {$e->getMessage()}" . PHP_EOL;
                 $loop->stop();
             });
         }
