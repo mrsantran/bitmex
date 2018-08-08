faa1770850b7b44602f12277de4f2387fe7a4cad
San Trần <laptrinhvien2013@gmail.com>
2018-08-08 10:48:25 +0700

    Add files via upload

--- bitmex.php
+++ bitmex.php
@@ -17,7 +17,7 @@ namespace BitmexPHP;
 /**
  * Main Bitmex class
  *
- * Eg. Usage:
+ * Eg. Usage: 
  * require 'vendor/autoload.php';
  * $api = new BitmexPHP\\API();
  */
@@ -25,14 +25,37 @@ class API {
 
     protected $ws = 'wss://www.bitmex.com/realtime?subscribe=';
     protected $ws_test = "wss://testnet.bitmex.com/realtime?subscribe=";
-    protected $api_key;
-    protected $api_secret;
     protected $env;
 
-    public function __construct($api_key = '', $api_secret = '', $env = true) {
-        $this->api_key = $api_key;
-        $this->api_secret = $api_secret;
+    public function __construct($env = true) {
         $this->env = $env ? $this->ws : $this->ws_test;
+    }
+
+    public function orderBook10($symbols, $callback) {
+        if (!is_array($symbols))
+            $symbols = [$symbols];
+        $loop = \React\EventLoop\Factory::create();
+        $react = new \React\Socket\Connector($loop);
+        $connector = new \Ratchet\Client\Connector($loop, $react);
+        foreach ($symbols as $symbol) {
+            $endpoint = 'orderBook10' . ':' . $symbol;
+            $connector($this->env . $endpoint)->then(function ( $ws ) use ($callback, $symbol, $loop, $endpoint ) {
+                $ws->on('message', function ( $data ) use ($ws, $loop, $symbol, $callback, $endpoint ) {
+                    $json = json_decode($data, true);
+                    if (isset($json["data"])) {
+                        call_user_func($callback, $this, $symbol, $json["data"]);
+                    }
+                });
+                $ws->on('close', function ( $code = null, $reason = null ) use ($symbol, $loop ) {
+                    echo "orderBook10({$symbol}) WebSocket Connection closed! ({$code} - {$reason})" . PHP_EOL;
+                    $loop->stop();
+                });
+            }, function ( $e ) use ($loop, $symbol ) {
+                echo "orderBook10({$symbol})) Could not connect: {$e->getMessage()}" . PHP_EOL;
+                $loop->stop();
+            });
+        }
+        $loop->run();
     }
 
     public function trades($symbols, $callback) {
